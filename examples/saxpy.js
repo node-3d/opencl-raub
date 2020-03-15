b2b940509c3bf779a473dac094347d1cd37ba39d
Luis Blanco <luisblanco1337@gmail.com>
2020-03-15 22:01:46 +0300

    Fix some examples

--- examples/saxpy.js
+++ examples/saxpy.js
@@ -1,145 +1,132 @@
-/*
-* @Author: mikael
-* @Date:   2015-09-22 21:13:24
-* @Last Modified by:   Mikael Bourges-Sevenier
-* @Last Modified time: 2018-02-24 19:30:06
-*/
-
 'use strict';
 
-var cl = require('../lib/opencl');
-var log = console.log;
+const cl = require('../');
 
-saxpy();
 
-function get_event_exec_time(event)
-{
-  // times are 64-bit values in nanoseconds. They are returned as [hi,lo] a 2-integer array
-  // here we use the lo parts since this example is unlikely to go beyond 2^31 nanseconds per event.
-  var start_time = cl.getEventProfilingInfo (event, cl.PROFILING_COMMAND_START);
-  var end_time=cl.getEventProfilingInfo (event, cl.PROFILING_COMMAND_END);
+const getEventExecTime = event => {
+	// times are 64-bit values in nanoseconds. They are returned as [hi,lo] a 2-integer array
+	// here we use the lo parts since this example is unlikely to go beyond 2^31 nanseconds per event.
+	let startTime = cl.getEventProfilingInfo (event, cl.PROFILING_COMMAND_START);
+	let endTime = cl.getEventProfilingInfo (event, cl.PROFILING_COMMAND_END);
 
-  return (end_time[1] - start_time[1]) * 1e-6; // report in millisecond (from nanoseconds)
+	return (endTime[1] - startTime[1]) * 1e-6; // report in millisecond (from nanoseconds)
+};
+
+let VECTOR_SIZE = 512 * 1024;
+
+console.log('SAXPY with vector size: ' + VECTOR_SIZE + ' elements');
+
+let alpha = 2.0;
+let A = new Float32Array(VECTOR_SIZE),
+	B = new Float32Array(VECTOR_SIZE),
+	C = new Float32Array(VECTOR_SIZE);
+
+for (let i = 0; i < VECTOR_SIZE; i++) {
+	A[i] = i;
+	B[i] = (VECTOR_SIZE - i);
+	C[i] = 0;
 }
 
-function saxpy() {
-  var VECTOR_SIZE = 512*1024;
-  log("SAXPY with vector size: "+VECTOR_SIZE+" elements");
+// create GPU context for this platform
+let context;
+if (cl.createContextFromType !== undefined) {
+	context = cl.createContextFromType(
+		[cl.CONTEXT_PLATFORM, cl.getPlatformIDs()[0]],
+		cl.DEVICE_TYPE_GPU,
+		null, null);
+}
+else {
+	let platform = cl.getPlatformIDs()[0];
+	let devices = cl.getDeviceIDs(platform, cl.DEVICE_TYPE_GPU);
+	console.info('Found ' + devices.length + ' GPUs: ');
+	let device = devices[0];
+	for (let i = 0;i < devices.length;i++) {
+		let name = cl.getDeviceInfo(devices[i],cl.DEVICE_NAME);
+		console.info('  Devices ' + i + ': ' + name);
+		if (name.indexOf('Intel') == -1) // prefer discrete GPU
+			device = devices[i];
+	}
 
-  var alpha = 2.0;
-  var A=new Float32Array(VECTOR_SIZE),
-      B=new Float32Array(VECTOR_SIZE),
-      C=new Float32Array(VECTOR_SIZE);
-
-  for(var i = 0; i < VECTOR_SIZE; i++)
-  {
-      A[i] = i;
-      B[i] = (VECTOR_SIZE - i);
-      C[i] = 0;
-  }
-
-  // create GPU context for this platform
-  var context;
-  if (cl.createContextFromType !== undefined) {
-    context = cl.createContextFromType(
-    [cl.CONTEXT_PLATFORM, cl.getPlatformIDs()[0]],
-    cl.DEVICE_TYPE_GPU,
-    null, null);
-  }
-  else {
-    var platform=cl.getPlatformIDs()[0];
-    var devices=cl.getDeviceIDs(platform, cl.DEVICE_TYPE_GPU);
-    console.info("Found "+devices.length+" GPUs: ");
-    var device=devices[0];
-    for(var i=0;i<devices.length;i++) {
-      var name=cl.getDeviceInfo(devices[i],cl.DEVICE_NAME);
-      console.info("  Devices "+i+": "+name);
-      if(name.indexOf("Intel")==-1) // prefer discrete GPU
-        device=devices[i];
-    }
-
-    context = cl.createContext(
-      [cl.CONTEXT_PLATFORM, platform],
-      [device]);
-  }
-
-  var device = cl.getContextInfo(context, cl.CONTEXT_DEVICES)[0];
-  log('using device: '+cl.getDeviceInfo(device, cl.DEVICE_NAME));
-
-  // Create command queue
-  var queue;
-  if (cl.createCommandQueueWithProperties !== undefined) {
-    queue = cl.createCommandQueueWithProperties(context, device, [
-      cl.QUEUE_PROPERTIES, cl.QUEUE_PROFILING_ENABLE
-    ]); // OpenCL 2
-  } else {
-    queue = cl.createCommandQueue(context, device, cl.QUEUE_PROFILING_ENABLE); // OpenCL 1.x
-  }
-
-  var saxpy_kernel_source = [
-    "__kernel                             ",
-    "void saxpy_kernel(float alpha,       ",
-    "                  __global float *A, ",
-    "                  __global float *B, ",
-    "                  __global float *C) ",
-    "{                                    ",
-    "    int idx = get_global_id(0);      ",
-    "    C[idx] = alpha* A[idx] + B[idx]; ",
-    "}                                    ",
-  ].join("\n");
-
-  //Create and program from source
-  var program=cl.createProgramWithSource(context, saxpy_kernel_source);
-
-  //Build program
-  cl.buildProgram(program);
-
-  var size=VECTOR_SIZE*Float32Array.BYTES_PER_ELEMENT; // size in bytes
-
-  // Create buffer for A and B and copy host contents
-  var aBuffer = cl.createBuffer(context, cl.MEM_READ_ONLY, size);
-  var bBuffer = cl.createBuffer(context, cl.MEM_READ_ONLY, size);
-
-  // Create buffer for C to read results
-  var cBuffer = cl.createBuffer(context, cl.MEM_WRITE_ONLY, size);
-
-  // Create kernel object
-  var kernel;
-  try {
-    kernel= cl.createKernel(program, "saxpy_kernel");
-  }
-  catch(err) {
-    console.log(program.getBuildInfo(device,cl.PROGRAM_BUILD_LOG));
-  }
-
-  // Set kernel args
-  cl.setKernelArg(kernel, 0, "float", alpha);
-  cl.setKernelArg(kernel, 1, "float*", aBuffer);
-  cl.setKernelArg(kernel, 2, "float*", bBuffer);
-  cl.setKernelArg(kernel, 3, "float*", cBuffer);
-
-  // Do the work
-  var write_events=[];
-  write_events[0]=cl.enqueueWriteBuffer (queue, aBuffer, false, 0, size, A, null, true);
-  write_events[1]=cl.enqueueWriteBuffer (queue, bBuffer, false, 0, size, B, null, true);
-
-  // Execute (enqueue) kernel
-  var localWS = null; // process one list at a time
-  var globalWS = [VECTOR_SIZE]; // process entire list
-
-  var kernel_event=cl.enqueueNDRangeKernel(queue, kernel, 1, null, globalWS, localWS, write_events, true);
-
-  // get results and block while getting them
-  var read_event=cl.enqueueReadBuffer (queue, cBuffer, false, 0, size, C, [kernel_event], true);
-
-  cl.waitForEvents([read_event]);
-  // cl.finish(queue);
-  log("C[last_value]="+C[VECTOR_SIZE-1]+" should be "+(2*(VECTOR_SIZE-1)+1));
-
-  // get all event statistics
-  log("Time to transfer matrix A: "+get_event_exec_time(write_events[0])+" ms");
-  log("Time to transfer matrix B: "+get_event_exec_time(write_events[1])+" ms");
-  log("Time to execute SAXPY kernel: "+get_event_exec_time(kernel_event)+" ms");
-  log("Time to read matrix C: "+get_event_exec_time(read_event)+" ms");
+	context = cl.createContext(
+		[cl.CONTEXT_PLATFORM, platform],
+		[device]);
 }
 
+let device = cl.getContextInfo(context, cl.CONTEXT_DEVICES)[0];
+console.log('using device: ' + cl.getDeviceInfo(device, cl.DEVICE_NAME));
+
+// Create command queue
+let queue;
+if (cl.createCommandQueueWithProperties !== undefined) {
+	queue = cl.createCommandQueueWithProperties(context, device, [
+		cl.QUEUE_PROPERTIES, cl.QUEUE_PROFILING_ENABLE
+	]); // OpenCL 2
+} else {
+	queue = cl.createCommandQueue(context, device, cl.QUEUE_PROFILING_ENABLE); // OpenCL 1.x
+}
+
+let saxpyKernelSource = [
+	'__kernel                             ',
+	'void saxpy_kernel(float alpha,       ',
+	'                  __global float *A, ',
+	'                  __global float *B, ',
+	'                  __global float *C) ',
+	'{                                    ',
+	'    int idx = get_global_id(0);      ',
+	'    C[idx] = alpha* A[idx] + B[idx]; ',
+	'}                                    ',
+].join('\n');
+
+//Create and program from source
+let program = cl.createProgramWithSource(context, saxpyKernelSource);
+
+//Build program
+cl.buildProgram(program);
+
+let size = VECTOR_SIZE * Float32Array.BYTES_PER_ELEMENT; // size in bytes
+
+// Create buffer for A and B and copy host contents
+let aBuffer = cl.createBuffer(context, cl.MEM_READ_ONLY, size);
+let bBuffer = cl.createBuffer(context, cl.MEM_READ_ONLY, size);
+
+// Create buffer for C to read results
+let cBuffer = cl.createBuffer(context, cl.MEM_WRITE_ONLY, size);
+
+// Create kernel object
+let kernel;
+try {
+	kernel = cl.createKernel(program, 'saxpy_kernel');
+}
+catch (err) {
+	console.console.log(program.getBuildInfo(device,cl.PROGRAM_BUILD_LOG));
+}
+
+// Set kernel args
+cl.setKernelArg(kernel, 0, 'float', alpha);
+cl.setKernelArg(kernel, 1, 'float*', aBuffer);
+cl.setKernelArg(kernel, 2, 'float*', bBuffer);
+cl.setKernelArg(kernel, 3, 'float*', cBuffer);
+
+// Do the work
+let writeEvents = [];
+writeEvents[0] = cl.enqueueWriteBuffer (queue, aBuffer, false, 0, size, A, null, true);
+writeEvents[1] = cl.enqueueWriteBuffer (queue, bBuffer, false, 0, size, B, null, true);
+
+// Execute (enqueue) kernel
+let localWS = null; // process one list at a time
+let globalWS = [VECTOR_SIZE]; // process entire list
+
+let kernelEvent = cl.enqueueNDRangeKernel(queue, kernel, 1, null, globalWS, localWS, writeEvents, true);
+
+// get results and block while getting them
+let readEvent = cl.enqueueReadBuffer (queue, cBuffer, false, 0, size, C, [kernelEvent], true);
+
+cl.waitForEvents([readEvent]);
+// cl.finish(queue);
+console.log('C[last_value]=' + C[VECTOR_SIZE - 1] + ' should be ' + (2 * (VECTOR_SIZE - 1) + 1));
+
+// get all event statistics
+console.log('Time to transfer matrix A: ' + getEventExecTime(writeEvents[0]) + ' ms');
+console.log('Time to transfer matrix B: ' + getEventExecTime(writeEvents[1]) + ' ms');
+console.log('Time to execute SAXPY kernel: ' + getEventExecTime(kernelEvent) + ' ms');
+console.log('Time to read matrix C: ' + getEventExecTime(readEvent) + ' ms');
--- examples/square.js
+++ examples/square.js
@@ -1,135 +1,115 @@
 'use strict';
 
-var cl = require("../lib/opencl");
-var fs = require("fs");
+const cl = require('../');
 
-function Square() {
-    console.log("Using Buffer");
+let fs = require('fs');
 
-    var ctx;
-    if (cl.createContextFromType !== undefined) {
-        ctx = cl.createContextFromType(
-            [cl.CONTEXT_PLATFORM, cl.getPlatformIDs()[0]], cl.DEVICE_TYPE_ALL, null, null);
-    }
-    else {
-        var platform = cl.getPlatformIDs()[0];
-        ctx = cl.createContext(
-            [cl.CONTEXT_PLATFORM, platform],
-            [cl.getDeviceIDs(platform, cl.DEVICE_TYPE_ALL)[0]]);
-    }
 
-    var NVALUES = 100;
-    var BYTES_PER_ELEMENT = Uint32Array.BYTES_PER_ELEMENT;
+console.log('Using Buffer');
 
-    var inputs = Buffer.alloc(NVALUES * BYTES_PER_ELEMENT); // *4 because uint is 4 bytes.
-    var outputs = Buffer.alloc(NVALUES * BYTES_PER_ELEMENT);
-    outputs.fill(0);
+let platform = cl.getPlatformIDs()[0];
+let ctx = cl.createContext(
+	[cl.CONTEXT_PLATFORM, platform],
+	[cl.getDeviceIDs(platform, cl.DEVICE_TYPE_ALL)[0]]);
 
-    // Note: using little endian for Intel-based machines, GPU follows same convention
-    // as CPU typically but it should be detected with clGetDeviceInfo(CL_DEVICE_ENDIAN_LITTLE)
+let NVALUES = 100;
+let BYTES_PER_ELEMENT = Uint32Array.BYTES_PER_ELEMENT;
 
-    for (var i = 0; i < NVALUES; ++i) {
-        inputs.writeUInt32LE(i, i * BYTES_PER_ELEMENT); // inputs[offset]=i with offset=i*4 since each uint32 value takes 4 bytes
-    }
+let inputs = Buffer.alloc(NVALUES * BYTES_PER_ELEMENT); // *4 because uint is 4 bytes.
+let outputs = Buffer.alloc(NVALUES * BYTES_PER_ELEMENT);
+outputs.fill(0);
 
-    var source = fs.readFileSync(__dirname + "/square.cl").toString();
+// Note: using little endian for Intel-based machines, GPU follows same convention
+// as CPU typically but it should be detected with clGetDeviceInfo(CL_DEVICE_ENDIAN_LITTLE)
 
-    var prog = cl.createProgramWithSource(ctx, source);
-
-    cl.buildProgram(prog);
-
-    var kern = cl.createKernel(prog, "square");
-
-    var inputsMem = cl.createBuffer(ctx, cl.MEM_READ_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT, inputs);
-    var outputsMem = cl.createBuffer(ctx, cl.MEM_WRITE_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT, outputs);
-
-    cl.setKernelArg(kern, 0, "uint*", inputsMem);
-    cl.setKernelArg(kern, 1, "uint*", outputsMem);
-    cl.setKernelArg(kern, 2, "uint", NVALUES);
-
-    var device = cl.getContextInfo(ctx, cl.CONTEXT_DEVICES)[0];
-    var cq;
-    if (cl.createCommandQueueWithProperties !== undefined) {
-        cq = cl.createCommandQueueWithProperties(ctx, device, []); // OpenCL 2
-    } else {
-        cq = cl.createCommandQueue(ctx, device, null); // OpenCL 1.x
-    }
-
-    cl.enqueueNDRangeKernel(cq, kern, 1, null, [NVALUES], null);
-
-    cl.enqueueReadBuffer(cq, outputsMem, true, 0, NVALUES * BYTES_PER_ELEMENT, outputs); // should contains i^2 for i=0,...,10000-1
-
-    cl.finish(cq);
-
-    console.log("#elems in outputs: " + outputs.length);
-    var last_value = outputs.readUInt32LE(BYTES_PER_ELEMENT * (NVALUES - 1));
-    console.log("Last value is : " + last_value + " should be " + ((NVALUES - 1) * (NVALUES - 1)));
+for (let i = 0; i < NVALUES; ++i) {
+	inputs.writeUInt32LE(i, i * BYTES_PER_ELEMENT);
+	// inputs[offset]=i with offset=i*4 since each uint32 value takes 4 bytes
 }
 
-function Square_TypedArray() {
-    console.log("Using TypedArrays");
+let source = fs.readFileSync(__dirname + '/square.cl').toString();
 
-    var ctx;
-    if (cl.createContextFromType !== undefined) {
-        ctx = cl.createContextFromType(
-            [cl.CONTEXT_PLATFORM, cl.getPlatformIDs()[0]], cl.DEVICE_TYPE_ALL, null, null);
-    }
-    else {
-        var platform = cl.getPlatformIDs()[0];
-        ctx = cl.createContext(
-            [cl.CONTEXT_PLATFORM, platform],
-            [cl.getDeviceIDs(platform, cl.DEVICE_TYPE_ALL)[0]]);
-    }
+let prog = cl.createProgramWithSource(ctx, source);
 
-    var NVALUES = 100;
-    var BYTES_PER_ELEMENT = Uint32Array.BYTES_PER_ELEMENT;
+cl.buildProgram(prog);
 
-    var inputs = new Uint32Array(NVALUES);
-    var outputs = new Uint32Array(NVALUES);
-    outputs.fill(0);
+let kern = cl.createKernel(prog, 'square');
 
-    for (var i = 0; i < NVALUES; ++i) {
-        inputs[i] = i;
-    }
+let inputsMem = cl.createBuffer(
+	ctx,
+	cl.MEM_READ_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT,
+	inputs
+);
+let outputsMem = cl.createBuffer(
+	ctx,
+	cl.MEM_WRITE_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT,
+	outputs
+);
 
-    var source = fs.readFileSync(__dirname + "/square.cl").toString();
+cl.setKernelArg(kern, 0, 'uint*', inputsMem);
+cl.setKernelArg(kern, 1, 'uint*', outputsMem);
+cl.setKernelArg(kern, 2, 'uint', NVALUES);
 
-    var prog = cl.createProgramWithSource(ctx, source);
-
-    cl.buildProgram(prog);
-
-    var kern = cl.createKernel(prog, "square");
-
-    var inputsMem = cl.createBuffer(ctx, cl.MEM_READ_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT, inputs);
-    var outputsMem = cl.createBuffer(ctx, cl.MEM_WRITE_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT, outputs);
-
-    cl.setKernelArg(kern, 0, "uint*", inputsMem);
-    cl.setKernelArg(kern, 1, "uint*", outputsMem);
-    cl.setKernelArg(kern, 2, "uint", NVALUES);
-
-    var device = cl.getContextInfo(ctx, cl.CONTEXT_DEVICES)[0];
-    var cq;
-    if (cl.createCommandQueueWithProperties !== undefined) {
-        cq = cl.createCommandQueueWithProperties(ctx, device, []); // OpenCL 2
-    } else {
-        cq = cl.createCommandQueue(ctx, device, null); // OpenCL 1.x
-    }
-
-    cl.enqueueNDRangeKernel(cq, kern, 1, null, [NVALUES], null);
-
-    cl.enqueueReadBuffer(cq, outputsMem, true, 0, NVALUES * BYTES_PER_ELEMENT, outputs); // should contains i^2 for i=0,...,10000-1
-
-    cl.finish(cq);
-
-    console.log("#elems in outputs: " + outputs.length);
-    var last_value = outputs[NVALUES - 1];
-    console.log("Last value is : " + last_value + " should be " + ((NVALUES - 1) * (NVALUES - 1)));
+let device = cl.getContextInfo(ctx, cl.CONTEXT_DEVICES)[0];
+let cq;
+if (cl.createCommandQueueWithProperties !== undefined) {
+	cq = cl.createCommandQueueWithProperties(ctx, device, []); // OpenCL 2
+} else {
+	cq = cl.createCommandQueue(ctx, device, null); // OpenCL 1.x
 }
 
-Square();
+cl.enqueueNDRangeKernel(cq, kern, 1, null, [NVALUES], null);
 
-console.log();
+cl.enqueueReadBuffer(cq, outputsMem, true, 0, NVALUES * BYTES_PER_ELEMENT, outputs);
+// should contains i^2 for i=0,...,10000-1
 
-Square_TypedArray();
+cl.finish(cq);
 
-console.log("\n== Program terminated ==");
+console.log('#elems in outputs: ' + outputs.length);
+let lastValue = outputs.readUInt32LE(BYTES_PER_ELEMENT * (NVALUES - 1));
+console.log('Last value is : ' + lastValue + ' should be ' + ((NVALUES - 1) * (NVALUES - 1)));
+
+console.log('Using TypedArrays');
+
+
+let inputs2 = new Uint32Array(NVALUES);
+let outputs2 = new Uint32Array(NVALUES);
+outputs2.fill(0);
+
+for (let i = 0; i < NVALUES; ++i) {
+	inputs2[i] = i;
+}
+
+let prog2 = cl.createProgramWithSource(ctx, source);
+
+cl.buildProgram(prog2);
+
+let kern2 = cl.createKernel(prog2, 'square');
+
+let inputsMem2 = cl.createBuffer(
+	ctx,
+	cl.MEM_READ_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT,
+	inputs2
+);
+let outputsMem2 = cl.createBuffer(
+	ctx,
+	cl.MEM_WRITE_ONLY | cl.MEM_COPY_HOST_PTR, NVALUES * BYTES_PER_ELEMENT,
+	outputs2
+);
+
+cl.setKernelArg(kern2, 0, 'uint*', inputsMem2);
+cl.setKernelArg(kern2, 1, 'uint*', outputsMem2);
+cl.setKernelArg(kern2, 2, 'uint', NVALUES);
+
+cl.enqueueNDRangeKernel(cq, kern, 1, null, [NVALUES], null);
+
+cl.enqueueReadBuffer(cq, outputsMem2, true, 0, NVALUES * BYTES_PER_ELEMENT, outputs2);
+// should contains i^2 for i=0,...,10000-1
+
+cl.finish(cq);
+
+console.log('#elems in outputs2: ' + outputs2.length);
+let lastValue2 = outputs2[NVALUES - 1];
+console.log('Last value is : ' + lastValue2 + ' should be ' + ((NVALUES - 1) * (NVALUES - 1)));
+
+console.log('DONE');
