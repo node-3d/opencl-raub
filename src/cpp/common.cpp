901061ccd20e3785f559a6bd8a603db2e5f59f30
Luis Blanco <luisblanco1337@gmail.com>
2025-03-31 17:57:22 +0400

    Investigate ubuntu cl 2

--- src/cpp/common.cpp
+++ src/cpp/common.cpp
@@ -20,67 +20,128 @@ void getPtrAndLen(Napi::Object obj, void** ptr, size_t *len) {
 
 const char* getExceptionMessage(const cl_int code) {
 	switch (code) {
-		CASE_RET(CL_SUCCESS, "Success!");
-		CASE_RET(CL_DEVICE_NOT_FOUND, "Device not found.");
-		CASE_RET(CL_DEVICE_NOT_AVAILABLE, "Device not available");
-		CASE_RET(CL_COMPILER_NOT_AVAILABLE, "Compiler not available");
-		CASE_RET(CL_MEM_OBJECT_ALLOCATION_FAILURE, "Memory object allocation failure");
-		CASE_RET(CL_OUT_OF_RESOURCES, "Out of resources");
-		CASE_RET(CL_OUT_OF_HOST_MEMORY, "Out of host memory");
-		CASE_RET(CL_PROFILING_INFO_NOT_AVAILABLE, "Profiling information not available");
-		CASE_RET(CL_MEM_COPY_OVERLAP, "Memory copy overlap");
-		CASE_RET(CL_IMAGE_FORMAT_MISMATCH, "Image format mismatch");
-		CASE_RET(CL_IMAGE_FORMAT_NOT_SUPPORTED, "Image format not supported");
-		CASE_RET(CL_BUILD_PROGRAM_FAILURE, "Program build failure");
-		CASE_RET(CL_MAP_FAILURE, "Map failure");
-		CASE_RET(CL_MISALIGNED_SUB_BUFFER_OFFSET, "Misaligned sub-buffer offset");
-		CASE_RET(CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST, "Execution status error for events in wait list");
-		CASE_RET(CL_COMPILE_PROGRAM_FAILURE, "Compile program failure");
-		CASE_RET(CL_LINKER_NOT_AVAILABLE, "Linker not available");
-		CASE_RET(CL_LINK_PROGRAM_FAILURE, "Link program failure");
-		CASE_RET(CL_DEVICE_PARTITION_FAILED, "Device partition failed");
-		CASE_RET(CL_KERNEL_ARG_INFO_NOT_AVAILABLE, "Kernel argument info not available");
-		CASE_RET(CL_INVALID_VALUE, "Invalid value");
-		CASE_RET(CL_INVALID_DEVICE_TYPE, "Invalid device type");
-		CASE_RET(CL_INVALID_PLATFORM, "Invalid platform");
-		CASE_RET(CL_INVALID_DEVICE, "Invalid device");
-		CASE_RET(CL_INVALID_CONTEXT, "Invalid context");
-		CASE_RET(CL_INVALID_QUEUE_PROPERTIES, "Invalid queue properties");
-		CASE_RET(CL_INVALID_COMMAND_QUEUE, "Invalid command queue");
-		CASE_RET(CL_INVALID_HOST_PTR, "Invalid host pointer");
-		CASE_RET(CL_INVALID_MEM_OBJECT, "Invalid memory object");
-		CASE_RET(CL_INVALID_IMAGE_FORMAT_DESCRIPTOR, "Invalid image format descriptor");
-		CASE_RET(CL_INVALID_IMAGE_SIZE, "Invalid image size");
-		CASE_RET(CL_INVALID_SAMPLER, "Invalid sampler");
-		CASE_RET(CL_INVALID_BINARY, "Invalid binary");
-		CASE_RET(CL_INVALID_BUILD_OPTIONS, "Invalid build options");
-		CASE_RET(CL_INVALID_PROGRAM, "Invalid program");
-		CASE_RET(CL_INVALID_PROGRAM_EXECUTABLE, "Invalid program executable");
-		CASE_RET(CL_INVALID_KERNEL_NAME, "Invalid kernel name");
-		CASE_RET(CL_INVALID_KERNEL_DEFINITION, "Invalid kernel definition");
-		CASE_RET(CL_INVALID_KERNEL, "Invalid kernel");
-		CASE_RET(CL_INVALID_ARG_INDEX, "Invalid argument index");
-		CASE_RET(CL_INVALID_ARG_VALUE, "Invalid argument value");
-		CASE_RET(CL_INVALID_ARG_SIZE, "Invalid argument size");
-		CASE_RET(CL_INVALID_KERNEL_ARGS, "Invalid kernel arguments");
-		CASE_RET(CL_INVALID_WORK_DIMENSION, "Invalid work dimension");
-		CASE_RET(CL_INVALID_WORK_GROUP_SIZE, "Invalid work group size");
-		CASE_RET(CL_INVALID_WORK_ITEM_SIZE, "Invalid work item size");
-		CASE_RET(CL_INVALID_GLOBAL_OFFSET, "Invalid global offset");
-		CASE_RET(CL_INVALID_EVENT_WAIT_LIST, "Invalid event wait list");
-		CASE_RET(CL_INVALID_EVENT, "Invalid event");
-		CASE_RET(CL_INVALID_OPERATION, "Invalid operation");
-		CASE_RET(CL_INVALID_GL_OBJECT, "Invalid OpenGL object");
-		CASE_RET(CL_INVALID_BUFFER_SIZE, "Invalid buffer size");
-		CASE_RET(CL_INVALID_MIP_LEVEL, "Invalid mip-map level");
-		CASE_RET(CL_INVALID_GLOBAL_WORK_SIZE, "Invalid global work size");
-		CASE_RET(CL_INVALID_PROPERTY, "Invalid property");
-		CASE_RET(CL_INVALID_IMAGE_DESCRIPTOR, "Invalid image descriptor");
-		CASE_RET(CL_INVALID_COMPILER_OPTIONS, "Invalid compiler options");
-		CASE_RET(CL_INVALID_LINKER_OPTIONS, "Invalid linker options");
-		CASE_RET(CL_INVALID_DEVICE_PARTITION_COUNT, "Invalid device partition count");
+		case CL_SUCCESS:
+			return "Success!";
+		case CL_DEVICE_NOT_FOUND:
+			return "Device not found.";
+		case CL_DEVICE_NOT_AVAILABLE:
+			return "Device not available";
+		case CL_COMPILER_NOT_AVAILABLE:
+			return "Compiler not available";
+		case CL_MEM_OBJECT_ALLOCATION_FAILURE:
+			return "Memory object allocation failure";
+		case CL_OUT_OF_RESOURCES:
+			return "Out of resources";
+		case CL_OUT_OF_HOST_MEMORY:
+			return "Out of host memory";
+		case CL_PROFILING_INFO_NOT_AVAILABLE:
+			return "Profiling information not available";
+		case CL_MEM_COPY_OVERLAP:
+			return "Memory copy overlap";
+		case CL_IMAGE_FORMAT_MISMATCH:
+			return "Image format mismatch";
+		case CL_IMAGE_FORMAT_NOT_SUPPORTED:
+			return "Image format not supported";
+		case CL_BUILD_PROGRAM_FAILURE:
+			return "Program build failure";
+		case CL_MAP_FAILURE:
+			return "Map failure";
+		case CL_MISALIGNED_SUB_BUFFER_OFFSET:
+			return "Misaligned sub-buffer offset";
+		case CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST:
+			return "Execution status error for events in wait list";
+		case CL_COMPILE_PROGRAM_FAILURE:
+			return "Compile program failure";
+		case CL_LINKER_NOT_AVAILABLE:
+			return "Linker not available";
+		case CL_LINK_PROGRAM_FAILURE:
+			return "Link program failure";
+		case CL_DEVICE_PARTITION_FAILED:
+			return "Device partition failed";
+		case CL_KERNEL_ARG_INFO_NOT_AVAILABLE:
+			return "Kernel argument info not available";
+		case CL_INVALID_VALUE:
+			return "Invalid value";
+		case CL_INVALID_DEVICE_TYPE:
+			return "Invalid device type";
+		case CL_INVALID_PLATFORM:
+			return "Invalid platform";
+		case CL_INVALID_DEVICE:
+			return "Invalid device";
+		case CL_INVALID_CONTEXT:
+			return "Invalid context";
+		case CL_INVALID_QUEUE_PROPERTIES:
+			return "Invalid queue properties";
+		case CL_INVALID_COMMAND_QUEUE:
+			return "Invalid command queue";
+		case CL_INVALID_HOST_PTR:
+			return "Invalid host pointer";
+		case CL_INVALID_MEM_OBJECT:
+			return "Invalid memory object";
+		case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:
+			return "Invalid image format descriptor";
+		case CL_INVALID_IMAGE_SIZE:
+			return "Invalid image size";
+		case CL_INVALID_SAMPLER:
+			return "Invalid sampler";
+		case CL_INVALID_BINARY:
+			return "Invalid binary";
+		case CL_INVALID_BUILD_OPTIONS:
+			return "Invalid build options";
+		case CL_INVALID_PROGRAM:
+			return "Invalid program";
+		case CL_INVALID_PROGRAM_EXECUTABLE:
+			return "Invalid program executable";
+		case CL_INVALID_KERNEL_NAME:
+			return "Invalid kernel name";
+		case CL_INVALID_KERNEL_DEFINITION:
+			return "Invalid kernel definition";
+		case CL_INVALID_KERNEL:
+			return "Invalid kernel";
+		case CL_INVALID_ARG_INDEX:
+			return "Invalid argument index";
+		case CL_INVALID_ARG_VALUE:
+			return "Invalid argument value";
+		case CL_INVALID_ARG_SIZE:
+			return "Invalid argument size";
+		case CL_INVALID_KERNEL_ARGS:
+			return "Invalid kernel arguments";
+		case CL_INVALID_WORK_DIMENSION:
+			return "Invalid work dimension";
+		case CL_INVALID_WORK_GROUP_SIZE:
+			return "Invalid work group size";
+		case CL_INVALID_WORK_ITEM_SIZE:
+			return "Invalid work item size";
+		case CL_INVALID_GLOBAL_OFFSET:
+			return "Invalid global offset";
+		case CL_INVALID_EVENT_WAIT_LIST:
+			return "Invalid event wait list";
+		case CL_INVALID_EVENT:
+			return "Invalid event";
+		case CL_INVALID_OPERATION:
+			return "Invalid operation";
+		case CL_INVALID_GL_OBJECT:
+			return "Invalid OpenGL object";
+		case CL_INVALID_BUFFER_SIZE:
+			return "Invalid buffer size";
+		case CL_INVALID_MIP_LEVEL:
+			return "Invalid mip-map level";
+		case CL_INVALID_GLOBAL_WORK_SIZE:
+			return "Invalid global work size";
+		case CL_INVALID_PROPERTY:
+			return "Invalid property";
+		case CL_INVALID_IMAGE_DESCRIPTOR:
+			return "Invalid image descriptor";
+		case CL_INVALID_COMPILER_OPTIONS:
+			return "Invalid compiler options";
+		case CL_INVALID_LINKER_OPTIONS:
+			return "Invalid linker options";
+		case CL_INVALID_DEVICE_PARTITION_COUNT:
+			return "Invalid device partition count";
+		case -1001:
+			return "OpenCL ICD failed";
 		default:
-			printf("Error: %d\n", code);
+			printf("OpenCL Unknown error: %d\n", code);
 			return "Unknown error";
 	}
 }
